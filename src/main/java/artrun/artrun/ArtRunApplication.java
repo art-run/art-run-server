af4386fedc031901d67a52a7f2ca56e41ac09869
zwan <2zwan@naver.com>
2022-01-17 17:21:57 +0900

    spring init

---
+++ src/main/java/artrun/artrun/ArtRunApplication.java
@@ -0,0 +1,13 @@
+package artrun.artrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtRunApplication.class, args);
+	}
+
+}
