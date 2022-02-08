adebffaf776670d501459a6270ce62bedf39ad8a
zwan <2zwan@naver.com>
2022-02-09 00:32:18 +0900

    feat(exception): customize InvalidWktException

--- src/main/java/artrun/artrun/global/error/exception/InvalidWktException.java
+++ src/main/java/artrun/artrun/global/error/exception/InvalidWktException.java
@@ -3,7 +3,7 @@ package artrun.artrun.global.error.exception;
 /**
  * org.locationtech.jts.io.ParseException 예외 전환
  */
-public class InvalidWktException extends RuntimeException {
+public class InvalidWktException extends InvalidValueException {
 
     public InvalidWktException(String msg) {
         super(msg);
