edf6318c90828779ad75ffebc5ca59ecd0d3af43
zwan <2zwan@naver.com>
2022-02-10 13:35:40 +0900

    chore: add @ApiOperation to Auth, Member

--- src/main/java/artrun/artrun/domain/auth/controller/AuthController.java
+++ src/main/java/artrun/artrun/domain/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package artrun.artrun.domain.auth.controller;
 import artrun.artrun.domain.auth.dto.*;
 import artrun.artrun.domain.auth.exception.AuthenticationException;
 import artrun.artrun.domain.auth.service.AuthService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +18,19 @@ import static artrun.artrun.global.error.exception.ErrorCode.UNAUTHORIZED;
 public class AuthController {
     private final AuthService authService;
 
+    @ApiOperation(value= "회원가입")
     @PostMapping("/signup")
     public ResponseEntity<SignupResponseDto> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         return ResponseEntity.ok(authService.signup(signupRequestDto));
     }
 
+    @ApiOperation(value= "로그인")
     @PostMapping("/login")
     public ResponseEntity<TokenResponseDto> login(@RequestBody @Valid LoginRequestDto memberRequestDto) {
         return ResponseEntity.ok(authService.login(memberRequestDto));
     }
 
+    @ApiOperation(value= "토큰 재발급")
     @PostMapping("/reissue")
     public ResponseEntity<TokenResponseDto> reissue(@RequestBody @Valid TokenRequestDto tokenRequestDto) {
         return ResponseEntity.ok(authService.reissue(tokenRequestDto));
--- src/main/java/artrun/artrun/domain/member/controller/MemberController.java
+++ src/main/java/artrun/artrun/domain/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package artrun.artrun.domain.member.controller;
 import artrun.artrun.domain.member.dto.MemberResponseDto;
 import artrun.artrun.domain.member.dto.SaveMemberRequestDto;
 import artrun.artrun.domain.member.service.MemberService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,22 +15,13 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    /**
-     * 멤버 정보 보기 (본인만)
-     * @param memberId
-     * @return
-     */
+    @ApiOperation(value= "멤버 정보 보기", notes = "본인만 확인 가능")
     @GetMapping("/{memberId}")
     public ResponseEntity<MemberResponseDto> getMember(@PathVariable Long memberId) {
         return ResponseEntity.ok(memberService.getMember(memberId));
     }
 
-    /**
-     * 멤버 정보 수정 (본인만)
-     * @param memberId
-     * @param saveMemberRequestDto
-     * @return
-     */
+    @ApiOperation(value = "멤버 정보 수정", notes = "본인만 수정 가능, 수정할 멤버 정보만 선택하여 넘기면 수정된 멤버 정보가 반환됨")
     @PutMapping("/{memberId}")
     public ResponseEntity<MemberResponseDto> saveMember(@PathVariable Long memberId,
                                                             @RequestBody SaveMemberRequestDto saveMemberRequestDto) {
