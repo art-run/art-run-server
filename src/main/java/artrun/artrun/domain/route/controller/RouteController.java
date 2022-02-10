5b64ab2810c82005d719f20c94e462164abf3090
zwan <2zwan@naver.com>
2022-02-10 13:42:22 +0900

    chore: add @ApiOperation to Route

--- src/main/java/artrun/artrun/domain/route/controller/RouteController.java
+++ src/main/java/artrun/artrun/domain/route/controller/RouteController.java
@@ -3,6 +3,8 @@ package artrun.artrun.domain.route.controller;
 import artrun.artrun.domain.route.dto.*;
 import artrun.artrun.domain.route.service.RouteFindService;
 import artrun.artrun.domain.route.service.RouteRunService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +18,20 @@ public class RouteController {
 
     private final RouteFindService routeFindService;
 
+    @ApiOperation(value= "routeId로 Route 정보 조회")
     @GetMapping("/{routeId}")
     public ResponseEntity<RouteResponseDto> get(@PathVariable Long routeId) {
         System.out.println("test");
         return ResponseEntity.ok(RouteResponseDto.of(routeFindService.get(routeId)));
     }
 
+    @ApiOperation(value="달리기 시작", notes = "목표 경로 그리기가 끝나고, 시작 버튼을 누를 때")
     @PostMapping("/start")
     public ResponseEntity<RouteStartResponseDto> start(@RequestBody RouteStartRequestDto routeStartRequestDto) {
         return ResponseEntity.ok(routeRunService.start(routeStartRequestDto));
     }
 
+    @ApiOperation(value="달리기 종료", notes = "달리기 종료 및 기록카드 작성 후, 저장 버튼을 누를 때")
     @PostMapping("/finish")
     public ResponseEntity<RouteFinishResponseDto> finish(@RequestBody RouteFinishRequestDto routeFinishRequestDto) {
         return ResponseEntity.ok(routeRunService.finish(routeFinishRequestDto));
