4ab2cb3aa062d6d47a54515496e65a18b50a89d3
zwan <2zwan@naver.com>
2022-02-12 22:22:49 +0900

    test(route): add getMyRoutes
    - fix getPublicRoutesFirst() typo

--- src/test/java/artrun/artrun/domain/route/repository/RouteRepositoryTest.java
+++ src/test/java/artrun/artrun/domain/route/repository/RouteRepositoryTest.java
@@ -39,9 +39,9 @@ class RouteRepositoryTest {
     @Autowired
     private RouteRepository routeRepository;
 
-    @DisplayName("getRoute 첫 페이지")
+    @DisplayName("getPublicRoutes 첫 페이지")
     @Test
-    void getRoutesFirst() {
+    void getPublicRoutesFirst() {
         // given
         Geometry route = wktToGeometry.wktToGeometry("LINESTRING (30 10, 10 30, 40 40)");
         Member member = Member.builder().id(1L).build();
@@ -71,6 +71,54 @@ class RouteRepositoryTest {
         assertThat(resultRoutes).hasSize(5);
         assertThat(resultRoutes.get(0).getTitle()).isEqualTo("title30");
         assertThat(resultRoutes.get(4).getTitle()).isEqualTo("title26");
+    }
+
+    @DisplayName("getMyRoutes 첫 페이지")
+    @Test
+    void getRoutesByMemberIdFirst() {
+        // given
+        Geometry route = wktToGeometry.wktToGeometry("LINESTRING (30 10, 10 30, 40 40)");
+        Member member = Member.builder().id(1L).build();
+        Member member2 = Member.builder().id(2L).build();
+        List<Route> routeList = new ArrayList<>();
+
+        for (int i = 1; i <= 30; i++) {
+            routeList.add(Route.builder()
+                    .id((long) i)
+                    .member(member)
+                    .targetRoute(route)
+                    .runRoute(route)
+                    .title("title" + (2*i-1))
+                    .distance(1000)
+                    .time(3600)
+                    .kcal(104)
+                    .color("000000")
+                    .thickness((byte) 3)
+                    .isPublic(true)
+                    .build());
+            routeList.add(Route.builder()
+                    .id((long) i+30)
+                    .member(member2)
+                    .targetRoute(route)
+                    .runRoute(route)
+                    .title("title" + (2*i))
+                    .distance(1000)
+                    .time(3600)
+                    .kcal(104)
+                    .color("000000")
+                    .thickness((byte) 3)
+                    .isPublic(true)
+                    .build());
+        }
+        routeRepository.saveAll(routeList);
+
+        // when
+        List<Route> resultRoutes = routeRepository.getRoutesByMemberId(1L, null);
+
+        // then
+        assertThat(resultRoutes).hasSize(5);
+        assertThat(resultRoutes.get(0).getTitle()).isEqualTo("title59");
+        assertThat(resultRoutes.get(4).getTitle()).isEqualTo("title51");
     }
 
 }
