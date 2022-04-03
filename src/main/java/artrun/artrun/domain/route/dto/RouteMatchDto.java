f8580d41afa4ccc8f910ede5962157160511fdb3
zwan <2zwan@naver.com>
2022-04-03 23:40:06 +0900

    fix: fix RouteMatchDto to use targetRoute

--- src/main/java/artrun/artrun/domain/route/dto/RouteMatchDto.java
+++ src/main/java/artrun/artrun/domain/route/dto/RouteMatchDto.java
@@ -2,19 +2,22 @@ package artrun.artrun.domain.route.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.locationtech.jts.geom.LineString;
 
 @Getter
 @Setter
 public class RouteMatchDto {
 
     private Long routeId;
+    private LineString targetRoute;
     private Double lat;
     private Double lng;
 
     @Override
     public String toString() {
-        return "RouteMatchRequestDto{" +
+        return "RouteMatchDto{" +
                 "routeId=" + routeId +
+                ", targetRoute=" + targetRoute +
                 ", lat=" + lat +
                 ", lng=" + lng +
                 '}';
